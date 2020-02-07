5db409e81792c3bcae86b5c9a9551f26ac82256c
ADMINISTRATOR <Mayankrathi33@gmail.com>
2020-02-07 14:58:45 +0530

    ADDED:Display winner between two player

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,64 +1,74 @@
-#!/bin/bash 
+#!/bin/bash -x
 echo "Welcome To Snake And Ladder Simulator"
 
-#CONSTANT
-WINING_POSITION=100
+#Constant
+WINNING_POSITION=100
 START_POSITION=0
 NO_PLAY=1
 LADDER=2
 SNAKE=3
-#VARIABLE
-die=0
-updatePosition=$START_POSITION	
 
-#For Rolling dice
-function dieRoll()
-{
-	die=$((RANDOM%6+1)); 
-	((dice++))
-}
-#Wining Position Till 100, dice count and it's dice position
+#Variables
+currentPlayer=$START_POSITION	
+player1=$START_POSITION
+player2=$START_POSITION
+flag=false
+
+#Check exact winning position 100
 function winingPosition(){
-	while [[ $updatePosition -lt $WINING_POSITION ]]
+	while [[ $currentPlayer -lt $WINNING_POSITION ]]
 	do
-		checkOption
-		echo "count die $dice  and position is $updatePosition"
+		checkPlayer
 	done
 }
-#Exact position Till 100
-function exactPositionForWining()
-{
-	if(($updatePosition>$WINING_POSITION))
-		then
-		updatePosition=$(($updatePosition-$die))      
-	fi
-}
-
-#If value Above WINING_POSITION THEN RESTART
-function resetToStart(){
-	if [[ $updatePosition -lt 0 ]]
-	then
-		updatePosition=$START_POSITION
-	fi
-}
-
 #Check for option And case 1)NO_PLAYE 2)LADDER 3)SNAKE
-function checkOption()
-{
+function checkOption(){
 	option=$((RANDOM%3+1))
-	dieRoll 
+	diceRoll=$((RANDOM%6+1))
 	case $option in
 		$NO_PLAY)
-			updatePosition=$updatePosition
+			currentPlayer=$currentPlayer
 			;;
 		$LADDER)
-			updatePosition=$(($updatePosition+die))
-			exactPositionForWining
+			currentPlayer=$(($currentPlayer+diceRoll))
+			if [[ $currentPlayer -gt $WINNING_POSITION ]]
+			then
+				currentPlayer=$(($currentPlayer-$diceRoll))
+			fi
 			;;
 		$SNAKE)
-			updatePosition=$(($updatePosition-die))
-			resetToStart
+			currentPlayer=$(($currentPlayer-diceRoll))
+			if [[ $currentPlayer -lt 0 ]]
+   			then
+				currentPlayer=$START_POSITION
+			fi
 			;;
 	esac
 }
+#Switch between two player
+function checkPlayer(){
+	if [[ $flag == false ]]
+	then
+		currentPlayer=$player1
+		checkOption
+		flag=true
+		player1=$currentPlayer
+	else
+		currentPlayer=$player2
+		checkOption
+		flag=false
+		player2=$currentPlayer
+	fi
+}
+
+#Check for winner between player1 and player2
+function winnerChecking(){
+        if [[ $player1 -eq $WINNING_POSITION ]]
+        then
+                echo "PLAYER 1 WIN"
+        else
+                echo "PLAYER 2 WIN"
+        fi
+}
 winingPosition
+winnerChecking
